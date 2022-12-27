2b1189ae0ed61bbd83d5a8a142d1f5304e74c96a
Toms Ilzbergs <ilzbergs@inbox.lv>
2022-12-27 16:02:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var ratings = document.querySelectorAll(".option");
+var firstStage = document.getElementById("first-stage");
+var secondStage = document.getElementById("second-stage")
+var selectedRate;
+
+
+
+ratings.forEach(function (e) {
+    e.onclick = function () {
+        selectedRate = this.innerHTML;
+    };
+});
+function submit() {
+    secondStage.style.display="flex";
+    firstStage.style.display="none";
+    document.getElementById("selected").innerHTML = `You selected ${selectedRate} out of 5`;
+}
